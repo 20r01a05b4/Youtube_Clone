d1ecdbaee6aa45af41935a0ac92cf2c02ec4fe47
20r01a05b4 <20r01a05b4@cmritonline.ac.in>
2024-01-04 14:01:34 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+
+//import { Provider } from 'react';
 import reportWebVitals from './reportWebVitals';
+//import {applyMiddleware,compose} from 'redux'
+//import { createStore } from 'redux';
+//import {thunk} from "redux-thunk"
+//import Reducers from './reducers';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+//const store=createStore(Reducers,compose(applyMiddleware(thunk)))
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
